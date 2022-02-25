2fca619934b98b7dff39733f29dea8ac32651882
YuqiCui <438049712@qq.com>
2022-02-25 21:00:29 +0800

    Delete pytsk directory

--- pytsk/__init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = [
-    "cluster",
-    "torch_model",
-    "utils"
-]
-__version__ = '0.1.0'
--- pytsk/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from .data import train_val_test_split, load_libsvm_format
-from .error import UnInitedError
-
-__all__ = [
-    "train_val_test_split",
-    "load_libsvm_format",
-    "UnInitedError",
-]
